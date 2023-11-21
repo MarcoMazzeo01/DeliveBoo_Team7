a775e3878161f8c0c7039a29ad857927ec67ff2f
plastikSun <marcom2k01@gmail.com>
2023-11-21 16:21:30 +0100

    create: restaurants migration

---
+++ database/migrations/2023_11_21_151237_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("vat", 11);
+            $table->text("description");
+            $table->text("image");
+            $table->string("address", 80);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
