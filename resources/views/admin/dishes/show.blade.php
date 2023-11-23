4fe87731da11dd6534f5abb7581c0a3cecbaed0e
RodiaGiordano <rodiagiordano93@gmail.com>
2023-11-23 11:05:07 +0100

    add show dish not visible

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -4,17 +4,16 @@
 
 
 <div class="container mt-5">
-    <div class="card" >
+    <div class="card border-danger border-2" >
         <img src="{{$dishDetail->image ? $dishDetail->image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png'}}" class="card-img-top py-3" alt="...">
       
         <div class="card-body">
           <h5 class="card-title">{{$dishDetail->name}}</h5>
-          <div class="card-text fw-bold">{{$dishDetail->address}}</div>
-          <p class="card-text">{{$dishDetail->description}}</p>
-          <div class="card-text fw-bold"> {{$dishDetail->price}}</div>
-          
-          
-        </div>
+          <p class="card-text">{{$dishDetail->getAbsDescription()}}</p>
+          <p class="card-text"><small class="fw-bold">Price: {{$dishDetail->price}}€</small></p>
+          @if(!$dishDetail->visible) 
+              <p class="card-text"><small class="fw-bold text-danger">Non disponibile</small></p>
+          @endif
       </div>
 </div>
 
