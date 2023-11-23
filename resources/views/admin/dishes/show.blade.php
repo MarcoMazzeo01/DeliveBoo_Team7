c209f76d776fbc130586df189b043ef44f813b49
RodiaGiordano <rodiagiordano93@gmail.com>
2023-11-23 17:16:01 +0100

    fix img after merge

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -6,7 +6,7 @@
 <div class="container mt-5">
   <a class="btn btn-primary" href="{{route('admin.dish.index')}}">Torna al menù</a>
     <div class="card {{$dishDetail->visible ? '' : 'border-danger'}} border-2" >
-        <img src="{{$dishDetail->image ? $dishDetail->image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png'}}" class="card-img-top py-3" alt="...">
+        <img src="{{asset('/storage/' . $dish->image)}}" class="card-img-top py-3" alt="...">
       
         <div class="card-body">
           <h5 class="card-title">{{$dishDetail->name}}</h5>
