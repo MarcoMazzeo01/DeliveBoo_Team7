215013b36326e6c6bb5910056543b7548a087b41
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-11-24 09:40:33 +0200

    add delete modal dish/restaurant

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -25,9 +25,40 @@
                                     <a href="{{route('admin.dish.edit', $dish)}}" class="me-2">
                                         <i class="fas fa-pencil-alt fa-lg"></i>
                                     </a>
-                                    <a href="#" class="me-2">
-                                        <i class="fas fa-trash-alt fa-lg"></i>
-                                    </a>
+
+                                   {{-- DELETE --}}
+                                   <a href="#" data-bs-toggle="modal" data-bs-target="#delete-modal-{{ $dish->id }}"
+                                    class="mx-1">
+                                    <i class="fa-solid fa-trash text-danger"></i>
+                                </a>
+                                {{-- Delete modal --}}
+                                <div class="modal fade" id="delete-modal-{{ $dish->id }}" tabindex="-1"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina ristorante</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Sei sicuro di voler eliminare la pietanza
+                                                "{{ $dish->name }}"?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Annulla</button>
+                                                {{-- Delete Form --}}
+                                                {{-- <form action="{{route('admin.restaurant.destroy', $dish)}}" method="POST" class="mx-1"> --}}
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-danger">Conferma</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 </div>
                             </div>
                             <div class="col-md-8">
