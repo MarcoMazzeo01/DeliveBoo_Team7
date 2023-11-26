ce586f0a699cd71c6f1daf36d3a1fe0745dc8724
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-11-26 12:01:20 +0100

    add navbar position fixed

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,52 +1,53 @@
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <div class="container">
-    
-    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
-      aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavId">
-      <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-        <li class="nav-item">
-          <a class="nav-link {{ Route::currentRouteName() == 'guest.home' ? 'active' : '' }}"
-            href="{{ route('guest.home') }}" aria-current="page">Home<span class="visually-hidden">(current)</span></a>
-        </li>
-        @guest
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-          </li>
-          @if (Route::has('register'))
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-          @endif
-        @else
-          <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-              data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              {{ Auth::user()->name }}
-            </a>
+<nav class="navbar navbar-expand-sm navbar-light bg-light customer_nav">
+    <div class="container">
 
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::currentRouteName() == 'guest.home' ? 'active' : '' }}"
+                        href="{{ route('guest.home') }}" aria-current="page">Home<span
+                            class="visually-hidden">(current)</span></a>
+                </li>
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-              </a>
-              
-              <li class="nav-item">
+                                {{ __('Logout') }}
+                            </a>
 
-                <a class="nav-link" href="{{ route('admin.restaurant') }}">My Restaurant</a>
-              </li>
+                    <li class="nav-item">
 
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-              </form>
+                        <a class="nav-link" href="{{ route('admin.restaurant') }}">My Restaurant</a>
+                    </li>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
             </div>
-          </li>
+            </li>
         @endguest
-      </ul>
-  
+        </ul>
+
     </div>
-  </div>
+    </div>
 </nav>
