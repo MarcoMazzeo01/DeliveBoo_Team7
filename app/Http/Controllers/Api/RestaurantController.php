81bdf315e5ece3d0d4cc711a8860b24343f89739
RodiaGiordano <rodiagiordano93@gmail.com>
2023-11-28 23:39:26 +0100

    complete Api restaurant with filter checkbox

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -22,17 +22,24 @@ class RestaurantController extends Controller
         // / ELIMINARE DOPO TEST ***************/
         // $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->with('types:id,name')->get();
         // / ELIMINARE DOPO TEST ***************/
+
+        if($request->input('id')){
         
-        $params = $request->input("Id");
+        $params = $request->input("id");
 
         $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->whereHas('types', function ($query) use ($params) {
             $query->whereIn('types.id', $params);
         })->with('types:id,name')->get();
-    
+
+        }else{
+
+            $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->with('types:id,name')->get();
+        }
+
         foreach ($restaurants as $restaurant){
             $restaurant->image = Storage::url($restaurant->image);
-            
         }
+            
        
         return response()->json($restaurants);
     }
@@ -49,4 +56,56 @@ class RestaurantController extends Controller
 
     
 }
+ 
+
+// <?php
+
+// namespace App\Http\Controllers\Api;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Support\Facades\Storage;
+
+// use App\Models\Restaurant;
+// use Illuminate\Http\Request;
+
+
+
+// class RestaurantController extends Controller
+// {
+//     /**
+//      * Display a listing of the resource.
+//      *
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function index(Request $request){
+        
+//         // / ELIMINARE DOPO TEST ***************/
+//         // $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->with('types:id,name')->get();
+//         // / ELIMINARE DOPO TEST ***************/
+        
+//         $params = $request->input("id");
+
+//         $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->whereHas('types', function ($query) use ($params) {
+//             $query->whereIn('types.id', $params);
+//         })->with('types:id,name')->get();
     
+//         foreach ($restaurants as $restaurant){
+//             $restaurant->image = Storage::url($restaurant->image);
+            
+//         }
+       
+//         return response()->json($restaurants);
+//     }
+
+
+
+//     public function show(){
+        
+    
+   
+//        return response()->json();
+       
+//     }
+
+    
+// }
