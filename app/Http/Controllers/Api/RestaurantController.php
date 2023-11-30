92b196b830878a423dddc941f5d4c2cad008387d
plastikSun <marcom2k01@gmail.com>
2023-12-01 00:38:15 +0100

    add: show call now also returns dish's restaurant_id

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -19,73 +19,67 @@ class RestaurantController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     // function per la generazione di path assoluti delle immagini
-   
-    public function index(Request $request){
-        
-        function imagePath($restaurants){
-            foreach ($restaurants as $restaurant){ 
-                
+    // function per la generazione di path assoluti delle immagini
+
+    public function index(Request $request)
+    {
+
+        function imagePath($restaurants)
+        {
+            foreach ($restaurants as $restaurant) {
+
                 $restaurant->image = Storage::url($restaurant->image);
-            }  
+            }
         }
-        
+
 
         $params = $request->input("filter"); // salvo i parametr in params
 
 
-        if(!empty($params)){ //verifico se sono stati passati parametri
-            
-           
-            if(is_array($params)){ //se è un array restituisco secondo le checkbox checked
+        if (!empty($params)) { //verifico se sono stati passati parametri
 
-                $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->whereHas('types', function ($query) use ($params) {
+
+            if (is_array($params)) { //se è un array restituisco secondo le checkbox checked
+
+                $restaurants = Restaurant::select('restaurants.id', 'restaurant_name', 'description', 'image', 'address')->whereHas('types', function ($query) use ($params) {
                     $query->whereIn('types.id', $params);
                 })->with('types:id,name')->get();
 
                 imagePath($restaurants);
-            }
-            else{ // altrimenti è una stringa
+            } else { // altrimenti è una stringa
 
-               
-                $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->whereRaw('LOWER(REPLACE(restaurant_name, " ", "")) LIKE ?', ['%'. $params .'%'])->with('types:id,name')->get();
+
+                $restaurants = Restaurant::select('restaurants.id', 'restaurant_name', 'description', 'image', 'address')->whereRaw('LOWER(REPLACE(restaurant_name, " ", "")) LIKE ?', ['%' . $params . '%'])->with('types:id,name')->get();
 
                 imagePath($restaurants);
-                
-
-                
             }
-            
-        }
-            
+        } else { // se assenza params restituisco tutti i ristoranti 
 
-        else{ // se assenza params restituisco tutti i ristoranti 
-            
 
-            $restaurants = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->with('types:id,name')->get();
-            
+            $restaurants = Restaurant::select('restaurants.id', 'restaurant_name', 'description', 'image', 'address')->with('types:id,name')->get();
+
 
 
             imagePath($restaurants);
         }
-  
-       
+
+
         return response()->json($restaurants);
     }
 
-    public function show($id){
-        
-        $restaurant = Restaurant::select('restaurants.id','restaurant_name', 'description', 'image', 'address')->where('id', $id)->with('types:id,name')->first();
-        $dishes = Dish::select('id','name','image','description','price','course_id')->where('restaurant_id',$id)->where('visible', '=', '1')->with('course:id,name,color')->get() ;
-        
+    public function show($id)
+    {
+
+        $restaurant = Restaurant::select('restaurants.id', 'restaurant_name', 'description', 'image', 'address')->where('id', $id)->with('types:id,name')->first();
+        $dishes = Dish::select('id', 'name', 'image', 'description', 'price', 'course_id', 'restaurant_id')->where('restaurant_id', $id)->where('visible', '=', '1')->with('course:id,name,color')->get();
+
 
         $restaurant->image = Storage::url($restaurant->image);
 
-        foreach ($dishes as $dish){
+        foreach ($dishes as $dish) {
             $dish->image = Storage::url($dish->image);
-        } 
+        }
 
-       return response()->json(['restaurant'=> $restaurant, 'dishes' => $dishes]);
-    }  
-    
+        return response()->json(['restaurant' => $restaurant, 'dishes' => $dishes]);
+    }
 }
