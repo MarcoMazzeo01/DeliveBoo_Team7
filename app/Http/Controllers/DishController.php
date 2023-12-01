b68361ba6b7c98586731235531b225f4bd9db4ec
RodiaGiordano <rodiagiordano93@gmail.com>
2023-12-01 20:06:57 +0100

    complete feature

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Dish;
 use App\Models\Course;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\support\Arr;
