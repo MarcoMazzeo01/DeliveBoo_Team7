d84811c6f1fd04940d4d66805501956e433e78c6
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-12-02 16:55:59 +0100

    add merchantId, public and private key

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->singleton(Gateway::class);
+        $this->app->singleton(Gateway::class, function ($app) {
+            return new Gateway(
+                [
+                    'enviroment' => 'sandbox',
+                    'merchantId' => 'wr53s5rwkkq7fwv9',
+                    'publicKey' => 'r3xqg3pmqy7p6s3s',
+                    'privateKey' => '1e7b4c08ee1681a77c63b15fc0871597',
+
+                ]
+            );
+        });
     }
 }
+// BraintreeGateway gateway = new BraintreeGateway(
+//     Environment.SANDBOX,
+//     "wr53s5rwkkq7fwv9",
+//     "r3xqg3pmqy7p6s3s",
+//     "1e7b4c08ee1681a77c63b15fc0871597"
+//   ); 
