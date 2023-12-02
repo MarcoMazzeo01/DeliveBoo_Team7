20cec275323a9d613f054259bc62340c23aab20d
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-12-02 17:30:31 +0100

    add makepayment api and orderRequest

--- app/Http/Controllers/Api/Orders/OrderController.php
+++ app/Http/Controllers/Api/Orders/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Orders;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,34 @@ class OrderController extends Controller
     {
         $token = $gateway->clientToken()->generate();
         $data = [
+            'succes' => true,
             'token' => $token
         ];
         return response()->json($data, 200);
     }
 
-    public function makePayment(Request $request, Gateway $gateway)
+    public function makePayment(OrderRequest $request, Gateway $gateway)
     {
-
+        $result = $gateway->transaction()->sale([
+            'amount' => '10.00',
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+        if ($result->success) {
+            $data = [
+                'succes' => true,
+                'message' => "Transazione esesguita"
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'succes' => false,
+                'message' => "Transazione fallita"
+            ];
+            return response()->json($data, 401);
+        }
     }
+
 }
