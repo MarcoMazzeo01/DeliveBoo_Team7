b3bec22e127c61aca94916770c7dd8eefb2d3e86
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-12-06 16:23:24 +0100

    add column email

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -14,27 +14,26 @@ use Faker\Factory;
 
 
 
-class OrderSeeder extends Seeder
-{
+class OrderSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run(Faker $faker, PhoneNumber $phoneNumber)
-    {
+    public function run(Faker $faker, PhoneNumber $phoneNumber) {
         $faker = Factory::create('it_IT');
 
         for($i = 0; $i < 10; $i++) {
 
             $order = new Order;
-            $order->customer_name = $faker->firstName($gender = 'male'|'female')  ;
-            $order->customer_surname =$faker->lastName() ;
+            $order->customer_name = $faker->firstName($gender = 'male' | 'female');
+            $order->customer_surname = $faker->lastName();
             $order->customer_phone = $phoneNumber->phoneNumber();
-            $order->address = $faker->address() ;
+            $order->address = $faker->address();
+            $order->customer_email = $faker->email();
             $order->notes = '';
-            $order->total = $faker->randomFloat(1, 40, 999) ;
-            
+            $order->total = $faker->randomFloat(1, 40, 999);
+
             $order->save();
         }
     }
