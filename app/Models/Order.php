0e92cbdbd48a62554ac8b6b03aa00d1e2c5f6c7e
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-12-08 12:19:21 +0200

    add ordersSSummary

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,9 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-
+    
     public function dishes()
     {
         return $this->belongsToMany(Dish::class);
     }
+    
+    public function getOrderDateAttribute()
+    {
+        return $this->created_at->format('d/m/Y');
+    }
+    
+    protected $dates = ['created_at'];
+   public function getOrderTimeAttribute()
+   {
+       return $this->created_at->format('H:i:s');
+   }
 }
