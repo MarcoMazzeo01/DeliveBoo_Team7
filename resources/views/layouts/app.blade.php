d89c7bd063857568b3420c6e5616a849f0564030
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-12-09 18:35:21 +0100

    add favicon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Deliveboo Partner</title>
+
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon" />
 
 
     <!-- Fonts -->
