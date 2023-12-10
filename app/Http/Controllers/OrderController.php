a122117e94ca8d14daa852b68130fce3eff76c9c
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-12-10 22:00:38 +0200

    order getting only associated orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -15,10 +16,17 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::orderBy('created_at', 'desc')->paginate(5);
-
-        $ordersDishes = Order::with('dishes')->get();
-
+        $user = Auth::user();
+    
+        $orders = Order::where('id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
+    
+        $ordersDishes = Order::where('id', $user->id)
+            ->with('dishes')
+            ->get();
+    
         return view("admin.orders.ordersSummary", compact('orders', 'ordersDishes'));
     }
+    
 }
